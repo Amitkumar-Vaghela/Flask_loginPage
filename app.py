e56e81ee50b56495d9b0057f079969ee7b97d736
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-18 19:16:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,6 @@ class RegisterForm(FlaskForm):
 def Home():
     return  render_template('home.html') 
 
-@app.route('/register')
-def register():
-    return  render_template('register.html') 
-
-@app.route('/login')
-def login():
-    return  render_template('login.html') 
-
 @app.route('/dashboard')
 def dashboard():
     return  render_template('dashboard.html') 
